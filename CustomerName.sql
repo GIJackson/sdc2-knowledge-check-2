3cd9c63585a5717a71bc9122c7b167e16434df37
Guillermo Jackson <gijcancer@gmail.com>
2022-11-06 21:51:30 -0500

    Two SQL files for questions 3 and 4.

---
+++ CustomerName.sql
@@ -0,0 +1,4 @@
+﻿SELECT o.OrderID, c.CustomerName
+FROM Orders o
+INNER JOIN Customers c ON o.CustomerID = c.CustomerID
+WHERE OrderID = 10310;
---
+++ SupplierAddress.sql
@@ -0,0 +1,4 @@
+﻿SELECT p.ProductID, s.Address, s.City, s.PostalCode, s.Country
+FROM Products p
+INNER JOIN Suppliers s ON p.SupplierID = s.SupplierID
+WHERE ProductID=40;
